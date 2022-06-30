5e05bb640e69506d338c3ee755e4950e2960411d
christian joshua <christianopeyemijoshua@gmail.com>
2022-06-30 15:14:20 -0700

    Task6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+printf("Size of a char: %d bytes(s)\n", size of (char));
+printf("Size of an int: %d bytes(s)\n", size of (int));
+printf("Size of long int: %d bytes(s)\n",size of (long int));
+printf("Size of float: %d bytes(s)\n",size of (float));
+return (0);
+
