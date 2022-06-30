1e18c40ea59cead9be116d02fffd9ce2cf9ea1e2
christian joshua <christianopeyemijoshua@gmail.com>
2022-06-30 16:24:37 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * Return: 0 if exited properly, non-zero otherwise
+ * main- main block
+ * Return: 0
  */
 int main(void)
 {
-char charType;
-int  intType;
-long int longIntType;
-long long int longLongType;
-float floatType;
+	char charType;
+	int intType;
+	long int longIntType;
+	long long int longLongType;
+	float floatType;
 
-printf("Size of a char: %ld bytes(s)\n", sizeof(charType));
-printf("Size of an int: %ld bytes(s)\n", sizeof(intType));
-printf("Size of a long int: %ld bytes(s)\n", sizeof(longIntType));
-printf("Size of a long long int: %ld bytes(s)\n" sizeof(longLongType);
-printf("Size of float: %ld bytes(s)\n", sizeof(floatType));
-return (0);
+	printf("Size of a char: %ld byte(s)\n", sizeof(charType));
+	printf("Size of an int: %ld byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(longIntType));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(longLongType));
+	printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
+	return (0);
 }
