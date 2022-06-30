6406e40f88024e3c21d06854efd2027e06171339
christian joshua <christianopeyemijoshua@gmail.com>
2022-06-30 11:32:24 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
- #include <stdio.h>
- #include <string.h>
+#include <stdio.h>
+#include <string.h>
 
  /**
  * main - Entry point
@@ -9,5 +9,5 @@
  int main(void)
  {
 	 puts("\"Programming is like building a multilingual puzzle");
-	 return (0);
+	  return (0);
  }
