1b65cd80ae3853cc9f3ef81f92971438cf6bb2ae
christian joshua <christianopeyemijoshua@gmail.com>
2022-06-30 17:03:21 -0700

    writing to the standard output

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - prints out the last part of a code in standard error
  * Return: 1 if sucess.
  */
 int main(void)
 {
-	write (2, "and  that piece of srt is useful\" - Dora Korpar , 2015-10-19\n", 59);
-	return(1);
+	write(2, "and  that piece of art is useful\" - Dora Korpar , 2015-10-19\n", 59);
+	return (1);
 }
-
-
