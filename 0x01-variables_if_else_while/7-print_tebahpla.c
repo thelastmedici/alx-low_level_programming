fc6904b298b30d86927b700a4fe4e39660ea2790
christian joshua <christianopeyemijoshua@gmail.com>
2022-07-02 08:18:37 -0700

    print reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse
+ *
+ * Return:0 if success
+ */
+int main(void)
+{
+ char var;
+ for (var = 'z'; var >= 'a'; var++);
+ {
+	putchar(var);
+ }
+ putchar('\n');
+ return (0);
+}
