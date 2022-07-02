db5e686d825305940fa1fdf10a8ac93b9d986564
christian joshua <christianopeyemijoshua@gmail.com>
2022-07-01 22:13:04 -0700

    small letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints all letter of the alphabet in lower case
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+	char var;
+
+	for (var ='a'; var <= 'z' var++);
+{
+	putchar(var);
+	
+}
+
+	putchar('\n');
+	return (0);
+
+}
