0f905ffdb1d4ba4280ff772b180e34bf08666e14
christian joshua <christianopeyemijoshua@gmail.com>
2022-07-02 12:18:48 -0700

    two two digit number

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,18 +5,18 @@
  * Return:0 if succesful
  *
  */
-int main (void)
+int main(void)
 	int var;
 {
 	for (var = 0; var < 99; var++)
 	{
 		int ii;
 
-		for (ii = var + 1; ii < 100, ii++)
+		for (ii = var + 1; ii < 100; ii++)
 		{
 			putchar((var / 10) + '0');
 			putchar((var % 10) + '0');
-			putchar('');
+			putchar(' ');
 			putchar((ii / 10) + '0');
 			putchar((ii % 10) + '0');
 
