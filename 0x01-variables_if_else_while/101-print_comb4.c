7b1148266736252c13d81218ccbb171419f6bd31
christian joshua <christianopeyemijoshua@gmail.com>
2022-07-02 11:14:43 -0700

    task11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/** 
+ * main -  prints all possible different combinations of three digits
+ *
+ * Return:0 if succesful
+ */
+int main(void)
+{
+	int var;
+	
+	for (var = 0; var < 8; var++)
+	{
+		int ii;
+
+		for (ii = var + 1;  ii < 9; ii++)
+		{
+			int jj;
+
+			for (jj = ii + 1; jj < 10; jj++)
+		{
+			putchar((var % 10) + '0');
+			putchar((ii % 10) + '0');
+			putchar((jj % 10) + '0');
+
+			if (var != 7)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			else
+				continue;
+		}
+	}
+      }
+	putchar('\n')
+	return (0);	
+}
