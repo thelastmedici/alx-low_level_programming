51498adbadb6807c349fb4dc13c121cf100cf388
christian joshua <christianopeyemijoshua@gmail.com>
2022-07-02 04:35:52 -0700

    number in base10

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,10 +8,12 @@
 int main(void)
 {
 	int var;
-	for (var = 0; var < 10; var++)
-	putchar((var % 10) + '0');
 
-	putchar('\n')
-	return (0);
+	for (var = 0; var < 10; var++)
+		putchar((var % 10; '0');
+
+		putchar('\n');
+
+		return(0);
 }
 
