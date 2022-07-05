68f7158c1e9b221d0359b2f78e9bf122b22672a3
christian joshua <christianopeyemijoshua@gmail.com>
2022-07-05 12:40:49 -0700

    print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,13 @@
  */
 int main(void)
 {
-	char var[]= "_putchar";
+	char var[] = "_putchar";
 	int ii;
 	int lenVar = sizeof(var) / sizeof(var[0]);
 
-	for (ii=0; ii < lenVar - 1; ii++)
+	for (ii = 0; ii < lenVar - 1; ii++)
 	{
 		_putchar(var[ii]);
-	
 	}
 		_putchar('\n');
 
