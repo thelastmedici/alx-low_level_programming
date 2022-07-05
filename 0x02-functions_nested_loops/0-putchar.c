f195efb1fbece2a8e0ffe4a5f2abca42669c5aa0
christian joshua <christianopeyemijoshua@gmail.com>
2022-07-05 12:24:19 -0700

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - this program prints _putchar
+ *
+ * Return:0 if succesful
+ */
+int main(void)
+{
+	char name[]="_putchar";
+	int ii;
+	int lenVar = sizeof(var) / sizeof(var[0]);
+
+	for (ii=0; ii < lenVar - 1; ii++)
+	{
+		_putchar(var[ii]);
+	
+	}
+		_putchar('\n');
+
+	return (0);
+
+}
+
