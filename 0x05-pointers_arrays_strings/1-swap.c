24ead76dc015718a0ff2339b544204885b67c250
christian joshua <christianopeyemijoshua@gmail.com>
2022-07-12 02:43:33 -0700

    swaps two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - takes two integer variable and swap them
+ * @var1: first variable
+ * @var2: second variable
+ *
+ * created by asiwaju
+ */
+
+void swap_int(int *var1, int *var2)
+{
+	int tmp = *var1;
+	*var1 = *var2;
+	*var2 = tmp;
+}
