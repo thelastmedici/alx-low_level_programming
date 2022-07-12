1794a423bd128a306adc7936236ec33dc240ed94
christian joshua <christianopeyemijoshua@gmail.com>
2022-07-12 02:51:36 -0700

    length of the string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strlen - prints the length of a string
+ * @s: string variable
+ *
+ * Return: length of the string
+ *
+ * created by asiwaju
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
