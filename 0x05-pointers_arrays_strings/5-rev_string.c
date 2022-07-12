c1b79b0b15634d0fca43ae2cc3d7292df3d7ff94
christian joshua <christianopeyemijoshua@gmail.com>
2022-07-12 05:31:01 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @str: string to be reversed
+ *
+ * dubbed by asiwaju
+ */
+void rev_string(char *str)
+{
+	int i = strlen(str) - 1, j = 0;
+	char tmp;
+
+	while (i > j)
+	{
+		tmp = str[i];
+		str[i] = str[j];
+		str[j] = tmp;
+		i--;
+		j++;
+	}
+}
+
