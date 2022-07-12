3c484b9e880299feff1102ac100400b56ad0af87
christian joshua <christianopeyemijoshua@gmail.com>
2022-07-12 04:51:41 -0700

    alphabet in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_rev - print string in reversed order
+ * @str: string to be reversed
+ *
+ * dubbed by asiwaju
+ */
+
+void print_rev(char *s)
+{
+	int i len = strlen(str);
+	for(i = len - 1; i >=0 i--)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
