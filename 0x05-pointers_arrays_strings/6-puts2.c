c8b264049b0d181467317b651d9b346c1e13a8d3
christian joshua <christianopeyemijoshua@gmail.com>
2022-07-12 06:39:17 -0700

    prints every other character of a string, starting with the first character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - print even number in a string
+ * @str: string variable
+ *
+ * dubbed by asiwaju
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i += 1;
+	}
+	_putchar('\n');
+}
+		
+
+	
+	
