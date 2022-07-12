a66f59c86ea1045623a73f6f86d20131c3196528
christian joshua <christianopeyemijoshua@gmail.com>
2022-07-12 04:55:05 -0700

    reverse string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,10 +8,10 @@
  * dubbed by asiwaju
  */
 
-void print_rev(char *s)
+void print_rev(char *str)
 {
-	int i len = strlen(str);
-	for(i = len - 1; i >=0 i--)
+	int i, len = strlen(str);
+	for(i = len - 1; i >=0; i--)
 		_putchar(str[i]);
 
 	_putchar('\n');
