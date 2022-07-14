361e38c37c788c1d01f21e33c16429c2073c5c2a
christian joshua <christianopeyemijoshua@gmail.com>
2022-07-14 13:30:28 -0700

    hot fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,7 +13,7 @@ int dest_len, i;
 for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
 ;
 
-for (i =0; i < n && src[i] != '\0'; i++)
+for (i = 0; i < n && src[i] != '\0'; i++)
 	dest[dest_len + i] = src[i];
 
 dest[dest_len + i] = '\0';
