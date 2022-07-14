2fa3d3499aa1c5a2eda7e7255af8c43965892ff5
christian joshua <christianopeyemijoshua@gmail.com>
2022-07-14 13:16:48 -0700

    concat string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,15 +1,23 @@
 #include "main.h"
-#include <string.h>
+#include 
 /**
-*_strncat -> function to append some charx
-*@dest: first param
-*@src: second param
-*@n: 3rd param
-*Return:string
+* *_strncat -> function to append some charx
+* @dest: first param
+* @src: second param
+* @n: 3rd param
+* Return:void
 */
 char *_strncat(char *dest, char *src, int n)
 {
-	strncat(*dest, *src, n);
-	return (dest);
-}
 
+int dest_len, i;
+for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
+;
+
+for (i =0; i < n && src[i] != '\0'; i++)
+	dest[dest_len + i] = src[i];
+
+dest[dest_len + i] = '\0';
+
+return (dest);
+}
