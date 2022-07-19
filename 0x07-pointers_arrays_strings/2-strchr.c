60c5e0ad73a601659c90c4b45dd21165b4d786d5
christian joshua <christianopeyemijoshua@gmail.com>
2022-07-19 06:58:27 -0700

    find a character

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,11 +1,12 @@
 #include <string.h>
 #include "main.h"
+
 /**
  * _strchr - find a character in a string
- * @s - pointer to the string to be searched
+ * @s: pointer to the string to be searched
  * @c: character to be found
  *
- * Return: a pointer to the first occurrence of the character
+ * Return: pointer to the first occurrence of the character c
  * dubbed by asiwaju
  */
 char *_strchr(char *s, char c)
