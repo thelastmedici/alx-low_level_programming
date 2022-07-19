f471791bd486bde60f4cbb872ad801a5635bcc3f
christian joshua <christianopeyemijoshua@gmail.com>
2022-07-19 09:17:05 -0700

    code fix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -15,15 +15,14 @@ void print_diagsums(int *a, int size)
 		if ((i % (size + 1) == 0) && (i % (size - 1) == 0)
 				&& i && i < size * size - 2)
 		{
-		sumDiag += a[i];
-		sumTrailDiag += a[i];
+			sumDiag += a[i];
+			sumTrailDiag += a[i];
 		}
 		else if (i % (size + 1) == 0)
 			sumDiag += a[i];
 		else if (i % (size - 1) == 0 && i && i < size * size - 2)
-		sumTrailDiag += a[i]
+			sumTrailDiag += a[i];
 	}
-
 	printf("%d, %d", sumDiag, sumTrailDiag);
 	putchar('\n');
 }
