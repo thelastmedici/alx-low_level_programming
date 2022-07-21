ca5d07ff29667752158f7c4bc67ef0e946383cd2
christian joshua <christianopeyemijoshua@gmail.com>
2022-07-20 17:02:26 -0700

    prints a string, followed by a new line.

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _puts_recursion - print a string rcursively
+ * @s: pointer to the string to be printed
+ *
+ */
+void _puts_recursion(char *s)
+{
+	if (strlen(s) < 1)
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
