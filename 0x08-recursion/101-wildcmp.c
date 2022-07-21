8a514954d3591d65c4cdbcce7317de360fbab6e1
christian joshua <christianopeyemijoshua@gmail.com>
2022-07-20 18:27:02 -0700

    check if two strings are identical

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -71,7 +71,7 @@ char *postfix_match(char *str, char *postfix)
  * @s1: The first string to be compared.
  * @s2: The second string to be compared - may contain wildcards.
  * Return: If the strings can be considered identical - 1.
- * 	 Otherwise - 0.
+ * Otherwise - 0.
  */
 int wildcmp(char *s1, char *s2)
 {
