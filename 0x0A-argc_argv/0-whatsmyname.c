f8a28b9bbd433547eca4144cce6ba93c3d80d4a5
christian joshua <christianopeyemijoshua@gmail.com>
2022-07-24 07:40:12 -0700

    print name followed by new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * @argc: number of actual arguements passed to main function
+ * @argv: pointer to the first element in the array of arguements
+ * Return: 0 if success
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	(void)argc;
+	exit(EXIT_SUCESS);
+}
