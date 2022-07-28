c837ae40b181dbfbd2d3ba127027a3ed4c9bd39e
christian joshua <christianopeyemijoshua@gmail.com>
2022-07-28 10:39:05 -0700

    concat string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * string_nconcat - Concatenates two strings using at most an inputted number of bytes.
+ * string_nconcat - Concatenates two strings using 
+ * 		at most an inputted number of bytes.
  * @s1: The first string.
  * @s2: The second string.
  * @n: The maximum number of bytes for s2 to concatenate to s1.
