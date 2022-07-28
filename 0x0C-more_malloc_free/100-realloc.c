28c9ffd7c5627f0014d9ef9a7caf8e9b8eafe6cf
christian joshua <christianopeyemijoshua@gmail.com>
2022-07-28 13:41:17 -0700

    memory reallocation

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -8,7 +8,7 @@
  *  @new_size: The size in bytes for the new memory block.
  *
  *  Return: If new_size == old_size - ptr.
- *  		If new_size == 0 and ptr is not NULL - NULL.
+ *		If new_size == 0 and ptr is not NULL - NULL.
  *	Otherwise - a pointer to the reallocated memory block.
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
