5c552989a67815f36602f460c7d935bfef79e42a
christian joshua <christianopeyemijoshua@gmail.com>
2022-08-01 21:54:31 -0700

    new type struct dog with the following elements:

--- main.h
+++ main.h
@@ -8,3 +8,7 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+
