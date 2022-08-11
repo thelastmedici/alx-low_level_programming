04daaa73de58cb1e46d2b2a47032b7fa973534b8
thelastmedici <christianopeyemijoshua@gmail.com>
2022-08-11 13:43:52 -0700

    fixing compilation error

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -11,14 +11,9 @@ void free_list(list_t *head)
 
 	while (head)
 	{
-		list_t *tmp;
-
-		while (head)
-		{
-			free(head->str);
-			tmp = head->next;
-			free(head);
-			head = tmp;
-		}
+		free(head->str);
+		tmp = head->next;
+		free(head);
+		head = tmp;
 	}
 }
