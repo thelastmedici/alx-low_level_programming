ab271f0d6796190672eb143bcf3d220a51c5a173
thelastmedici <christianopeyemijoshua@gmail.com>
2022-08-16 06:29:30 -0700

    print out the number of element in a list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,14 +7,14 @@
  * Return: the number of nodes
  */
 
-size_t print_listint(const listint_t *h);
+size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		h = h->n;
+		h = h->next;
 		count++;
 	}
 	return (count);
