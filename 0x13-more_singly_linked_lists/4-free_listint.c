7d2b79d5dc8bac1eb785c1d8c22fa3c955612932
thelastmedici <christianopeyemijoshua@gmail.com>
2022-08-16 09:08:41 -0700

    free a list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,14 +1,15 @@
 #include "lists.h"
-
 /**
  * free_listint - free a linked list
- * @head - pointer to the adress of the first node in the list
+ * @head:  pointer to the  head node in the list
  */
 
-void free_listint(listint_t *head);
+void free_listint(listint_t *head)
 {
 	listint_t *tmp;
+
 	while (head)
+
 	{
 		tmp = head->next;
 		free(head);
