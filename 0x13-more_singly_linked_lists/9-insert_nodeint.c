929a6d24f9ee9c63585c2d349b1ca4ae1c3f760f
thelastmedici <christianopeyemijoshua@gmail.com>
2022-08-16 13:03:22 -0700

    insert a new node in a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,8 +29,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		(*head) = new;
 		return (new);
 	}
-	
-	current = *head;
+
+		current = *head;
 
 	while (idx != 1)
 	{
