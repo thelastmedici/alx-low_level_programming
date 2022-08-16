6fabfbaffc4ca012e939887cdc1cc2bd871d6509
thelastmedici <christianopeyemijoshua@gmail.com>
2022-08-16 04:52:42 -0700

    function that print out the element of list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "list.h"
+
+/**
+ * print_listint - print all element of listint_t
+ * @h: reference to the head list
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_listint(const listint_t *h);
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+	       	h = h->next;
+		count++;
+	}
+}	
