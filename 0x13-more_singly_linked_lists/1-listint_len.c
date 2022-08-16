ae47aede270739032db87e240bca5b8527481864
thelastmedici <christianopeyemijoshua@gmail.com>
2022-08-16 06:02:10 -0700

    return the number of element in a list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,15 +1,15 @@
 #include "lists.h"
 /**
  * listint_len - length of the string in the list
- * @h - pointer to the head of the list
+ * @h: pointer to the head of the list
  *
- * Return: t2he number of element
+ * Return: the number of element
  */
 
-size_t listint_len(const listint_t *h);
+size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
-		while(h)
+		while (h)
 		{
 			count++;
 			h = h->next;
