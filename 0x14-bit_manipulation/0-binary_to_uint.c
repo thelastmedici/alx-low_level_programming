ab9f00215961cecf9bf89294ca11b93403e665f9
thelastmedici <christianopeyemijoshua@gmail.com>
2022-08-18 14:07:20 -0700

    converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,24 +9,24 @@
  *
  * Return: converted number if successful, 0 otherwise
  */
-
 unsigned int binary_to_uint(const char *b)
 {
 	int i, base = 1, len = 0;
-	unsigned res = 0;
+	unsigned int res = 0;
 
 	if (!b)
 		return (0);
-	len = strlen(b);
 
+	len = strlen(b);
 
 	for (i = len - 1; i >= 0; i--)
 	{
-		if (b[i] < '0' && b[i] > '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
 		if (b[i] == '1')
 			res += base;
 		base *= 2;
 	}
+
 	return (res);
 }
