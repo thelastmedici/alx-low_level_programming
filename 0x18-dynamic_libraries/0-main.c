7c51c972dcd89a9292324324e404a9e7d9b0b322
thelastmedici <christianopeyemijoshua@gmail.com>
2022-10-03 16:08:04 -0700

    dynamic library

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*main - check the code
+* Return:  EXIT_SUCCESS.
+*/
+int main(void)
+{
+printf("%d\n", _strlen("My Dynamic Lib"));
+return (EXIT_SUCCESS);
+}
